167176fa04ae927e35118299b625a6139a2a43ac
haohonglong <947923503@qq.com>
2018-05-22 16:03:06 +0800

    add

---
+++ material-ui/my-app/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Layout from './components/Layout';
+
+
+function App() {
+    return (
+        <Layout>
+
+        </Layout>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
